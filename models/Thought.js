f42c96bf7b0bed13660db244fa576cd42e278867
Thomas Munzar <tmunzar92@gmail.com>
2023-08-22 15:08:51 -0700

    initial commit

---
+++ models/Thought.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText:{ type:String, required: true}
+    reaction: [reactionSchema]
+})
+
+const reactionSchema = new mongoose.Schema({
+//     * `reactionId`
+//   * Use Mongoose's ObjectId data type
+//   * Default value is set to a new ObjectId
+
+// * `reactionBody`
+//   * String
+//   * Required
+//   * 280 character maximum
+
+// * `username`
+//   * String
+//   * Required
+
+// * `createdAt`
+//   * Date
+//   * Set default value to the current timestamp
+//   * Use a getter method to format the timestamp on query
+})
+
+
+// **Thought**:
+
+// * `thoughtText`
+//   * String
+//   * Required
+//   * Must be between 1 and 280 characters
+
+// * `createdAt`
+//   * Date
+//   * Set default value to the current timestamp
+//   * Use a getter method to format the timestamp on query
+
+// * `username` (The user that created this thought)
+//   * String
+//   * Required
+
+// * `reactions` (These are like replies)
+//   * Array of nested documents created with the `reactionSchema`
+
+// **Schema Settings**:
+
+// Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+
+
+//

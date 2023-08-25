6e649a9d2ab12b8ae0cb2d8aa2907c7a697f20ef
Thomas Munzar <tmunzar92@gmail.com>
2023-08-24 17:37:54 -0700

    finised writing Schemas and models

--- models/Thought.js
+++ models/Thought.js
@@ -1,31 +1,69 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        //Defaulst value set to new Object
+        default: Schema.Types.ObjectId
+        //Thie default is essentially telling mongoose to use its own unique objectId's to organize the new reactions. we are not giving them unique ids.
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+ 
+});
 
 
-const thoughtSchema = new mongoose.Schema({
-    thoughtText:{ type:String, required: true}
+const thoughtSchema = new Schema({
+
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+        //??
+    },
+
+    username:
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+
     reaction: [reactionSchema]
-})
+});
 
-const reactionSchema = new mongoose.Schema({
-//     * `reactionId`
-//   * Use Mongoose's ObjectId data type
-//   * Default value is set to a new ObjectId
+// Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+thoughtSchema.
+virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+});
 
-// * `reactionBody`
-//   * String
-//   * Required
-//   * 280 character maximum
+//Turn our thoughtSchema into a model so users can interact with it (constructor function)
+//We are creating a model named Thought based on the thoughtSchema, this model allows methods like find, create, findOneAndUpdate... ect
+const Thought = model('thought', thoughtSchema)
 
-// * `username`
-//   * String
-//   * Required
 
-// * `createdAt`
-//   * Date
-//   * Set default value to the current timestamp
-//   * Use a getter method to format the timestamp on query
-})
-
+module.exports = Thought;
 
 // **Thought**:
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,42 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            //Need to validate email with validator
+        },
+        thoughts: [{ type: Schema.Types.ObjectId, ref: "thought" }],
+
+        friends: [{ type: Schema.Types.ObjectId, ref: 'friend' }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return `${this.friends.length}`
+    });
+
+// referencing friends will be similiar to ^ above
+
+const User = model('user', userSchema)
+
+module.exports = User;
+
 // create the schema
 
 // **User**:
